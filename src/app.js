a181bb941e7cc56b9eb7405524b9681250800e62
Fayzillo <ovovovlululutvata@gmail.com>
2025-05-30 23:01:26 +0500

    cors

--- src/app.js
+++ src/app.js
@@ -5,12 +5,15 @@ import errorHandler from './middleware/errHeandler.js'
 import router from './router/router.js'
 import fileUpload from 'express-fileupload'
 import { swaggerUi, swaggerSpec } from "./config/swagger.js";
+import cors from "cors"
+
 const app = express()
 const PORT = process.env.PORT || 4000
 
-
+app.use(cors)
 app.use(express.json())
 app.use(fileUpload())
+
 await connectDB()
 app.use('/v1/api',router)
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
